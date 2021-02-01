7734c581b35195689df723a1b9fd157eb9a1da3f
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2021-02-01 12:16:23 +0000

    Validation tests

--- tests/testthat/test-Constructors.R
+++ tests/testthat/test-Constructors.R
@@ -109,17 +109,121 @@ test_that("Can create object or return correct error messages", {
                                   scale.param = 5,
                                   truncation = 25,
                                   cov.param = cov.params),
-               "Unsupported key function.")
+               "Unsupported key function, please select hn, hr or uf.")
 
 
   cov.params <- list(size = log(1.05),
+                     sex = data.frame(level = c("male", "female"),
+                                      param = c(log(1), log(1.5))),
+                     height = NULL)
+
+  expect_error(make.detectability(key.function = "hn",
+                               scale.param = 5,
+                               truncation = 25,
+                               cov.param = cov.params),
+               "List element height of the cov.param list does not contain any values.")
+
+  cov.params <- list(size = log(1.05),
                      sex = data.frame(factor = c("male", "female"),
+                                      param = c(log(1), log(1.5))))
+
+  expect_error(make.detectability(key.function = "hn",
+                                  scale.param = 5,
+                                  truncation = 25,
+                                  cov.param = cov.params),
+               "The dataframe for covariate 'sex' has missing columns: level")
+
+  cov.params <- list(size = log(1.05),
+                     sex = data.frame(level = rep(c("male", "female"),2),
+                                      param = rep(c(log(1), log(1.5)),2),
+                                      strata = c("A", "A", "B", "B"),
+                                      ignore = rep(10,4)))
+
+  expect_warning(test <- make.detectability(key.function = "hn",
+                                             scale.param = 5,
+                                             truncation = 25,
+                                             cov.param = cov.params),
+                  "The dataframe for covariate 'sex' has unrecognised columns: ignore. These will be ignored.")
+  tmp <- data.frame(level = rep(c("male", "female"),2),
+                    param = rep(c(log(1), log(1.5)),2),
+                    strata = c("A", "A", "B", "B"))
+  expect_equal(test@cov.param$sex, tmp)
+
+  cov.params <- list(size = log(1.05))
+  cov.params[[2]] <- data.frame(factor = c("male", "female"),
+                                param = c(log(1), log(1.5)))
+
+  expect_error(make.detectability(key.function = "hn",
+                                  scale.param = 5,
+                                  truncation = 25,
+                                  cov.param = cov.params),
+               "Not all the elements of the cov.param list are named. Please provide names for all elements.")
+
+  expect_warning(test <- make.detectability(key.function = "hn",
+                                            scale.param = 5,
+                                            shape.param = 1,
+                                            truncation = 25),
+                 "You have selected the hn key function and supplied a shape parameter value, this will be ignored.")
+
+  expect_error(make.detectability(key.function = "hr",
+                                  scale.param = 5,
+                                  truncation = 25),
+               "You have selected the hazard rate model but not supplied a shape parameter.")
+
+  expect_error(make.detectability(key.function = "hr",
+                                  scale.param = 5,
+                                  shape.param = c(1,1.5),
+                                  truncation = 25),
+               "The same number of values must be provided for both the shape and scale parameters or only one value supplied for either the shape or scale parameter.")
+
+  cov.params <- list(size = log(1.05),
+                     sex = data.frame(level = c("male", "female"),
                                       param = c(log(1), log(1.5))))
 
   detect <- make.detectability(key.function = "hn",
                                scale.param = 5,
                                truncation = 25,
                                cov.param = cov.params)
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Test analysis creation
+
+  # Truncation checks
+  expect_error(make.ds.analysis(dsmodel = list(~1, ~size, ~size+sex),
+                                key = rep("hn",3),
+                                cutpoints = seq(0,25, length = 4),
+                                truncation = "15%"),
+               "Truncation cannot be supplied as a percentage with binned data.")
+
+  expect_error(make.ds.analysis(dsmodel = list(~1, ~size, ~size+sex),
+                                key = rep("hn",3),
+                                cutpoints = seq(0,25, length = 4),
+                                truncation = list(left="1",right="15%")),
+               "Truncation cannot be supplied as a percentage with binned data.")
+
+  expect_error(make.ds.analysis(dsmodel = list(~1, ~size, ~size+sex),
+                                key = rep("hn",3),
+                                cutpoints = seq(0,25, length = 4),
+                                truncation = list(left="1",righ="15%")),
+               "Truncation must be supplied as a single number/string or a list with elements \"left\" and \"right\".")
+
+  expect_error(make.ds.analysis(dsmodel = list(~1, ~size, ~size+sex),
+                                key = rep("hn",3),
+                                cutpoints = seq(0,25, length = 4),
+                                truncation = c("1","15%")),
+               "Truncation must be supplied as a single number/string or a list with elements \"left\" and \"right\".")
+
+
+
+
+
+
+
+
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Test simulation creation
+  #check number of shape / scale parameters is equal to number of strata - what else needs to be equal to strata numbers?
 
 
 })
