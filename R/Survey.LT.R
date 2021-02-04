7b0ed418ca9f19140359bb94c2b45dc93fb062f1
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2021-02-04 23:46:38 +0000

    Move survey plot method - going to need fully implementing
    For some reason the env. is getting messed up and cannot find the plot functions defined in dssd so going to need to re-implement here.. grrr!

--- R/Survey.LT.R
+++ R/Survey.LT.R
@@ -82,36 +82,3 @@ setMethod(
 )
 
 
-#' @export
-setMethod(
-  f="plot",
-  signature=c("Survey.LT"),
-  definition=function(x, y = NULL, ...){
-    args <- list(...)
-    if(!"region" %in% names(args)){
-      stop("The region must be supplied in order to plot the survey.", call. = FALSE)
-    }
-    region <- args$region
-    orig.opts <- par(mfrow = c(2,2))
-    on.exit(par(orig.opts))
-    # Plot transects
-    plot(region, x@transect, main = "Transects")
-    # Plot population
-    plot(region, main = "Population")
-    pop <- x@population@population
-    points(pop$x, pop$y, pch = 20, col = 2)
-    # Plot detections
-    plot(region, x@transect, main = "Survey")
-    pop <- x@population@population
-    points(pop$x, pop$y, pch = 20, col = 1)
-    dist.data <- x@dist.data
-    ids <- na.omit(dist.data$individual)
-    detects <- pop[pop$individual %in% ids,]
-    points(detects$x, detects$y, pch = 20, cex = 1.5, col = 6)
-    # Plot histogram of detections
-    hist(dist.data$distance, main = "Detection Distances", xlab = "Distance from the transect")
-    return(invisible(x))
-  }
-)
-
-
