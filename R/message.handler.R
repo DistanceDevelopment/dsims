1f3b8b3780797dccf12f2512fc3928337fb4731e
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2021-03-02 13:32:17 +0000

    Removing unnecessary print statements

--- R/message.handler.R
+++ R/message.handler.R
@@ -1,10 +1,9 @@
-message.handler <- function(warnings, message=character(0), summarise = FALSE){
+message.handler <- function(warnings, message=character(0)){
   # This function either stores or displays a summary of warnings
   # and errors.
   # ARGUMENTS
   #   - warnings is a list of two elements (message and counter)
   #   - message is the new message if one exists
-  #   - summarise if TRUE it prints out a summary of the warnings
   if(length(message) > 0){
     # Check if the warning already exists
     message <- paste(message, collapse = " ")
@@ -15,11 +14,6 @@ message.handler <- function(warnings, message=character(0), summarise = FALSE){
       warnings$counter[[no.warnings + 1]] <- 1
     }else{
       warnings$counter[[index]] <- warnings$counter[[index]] + 1
-    }
-  }else if(summarise){
-    print("/nErrors//Warnings /n")
-    for(i in seq(along = warnings$message)){
-      cat(paste(warnings$message[[i]], "occured", warnings$counter[[i]], "time(s)", sep = " "))
     }
   }
   return(warnings)
