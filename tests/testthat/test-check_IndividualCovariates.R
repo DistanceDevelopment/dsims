11c1f5c1b9e948c63e7706d89418f23034648987
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2022-07-06 01:03:45 +0100

    Add tests

--- tests/testthat/test-check_IndividualCovariates.R
+++ tests/testthat/test-check_IndividualCovariates.R
@@ -65,3 +65,84 @@ test_that("Can deal with different covariate options", {
   expect_true(inherits(summary.sim, "Simulation.Summary"))
 
 })
+
+test_that("Can run simulation with cluster size", {
+  
+  #Set up simulatio
+  region <- make.region(region.name = "main")
+  
+  density <- make.density(region = region)
+  
+  covariate.list <- list()
+  covariate.list$size <- list(distribution = "ztruncpois", mean = 5)
+  pop.descrp <- make.population.description(region = region,
+                                            density = density,
+                                            covariates = covariate.list,
+                                            N = 250)
+  
+  detect <- make.detectability()
+  
+  design <- make.design(region = region)
+  
+  expect_error(make.ds.analysis(dfmodel = list(~1),
+                               key = c("hn","hn"),
+                               truncation = 50),
+               "All models must be unique, there appears to be the same combination of key function and dfmodel entered more than once.")
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1, ~size),
+                               key = c("hn","hn"),
+                               truncation = 50)
+  expect_equivalent(analyses@dfmodel, list(~1, ~size))
+  
+  expect_error(make.ds.analysis(dfmodel = list(~size, ~size),
+                               key = c("hr","hr"),
+                               truncation = 50),
+               "All models must be unique, there appears to be the same combination of key function and dfmodel entered more than once.")
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1, ~size,~1, ~size),
+                               key = c("hn","hn","hr","hr"),
+                               truncation = 50)
+  expect_equivalent(analyses@dfmodel, list(~1, ~size,~1, ~size))
+  
+  expect_error(make.ds.analysis(dfmodel = list(~1, ~1, ~1),
+                                key = c("hn","hr", "hn"),
+                                truncation = 50),
+               "All models must be unique, there appears to be the same combination of key function and dfmodel entered more than once.")
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1),
+                               key = "hn",
+                               truncation = 50)
+  
+  sim <- make.simulation(reps = 3,
+                         design = design,
+                         population.description = pop.descrp,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  
+  survey <- run.survey(sim)
+  
+  sim.tmp <- run.simulation(sim)
+  expect_true(inherits(sim.tmp, "Simulation"))
+  
+  summary.sim <- summary(sim.tmp, description.summary = FALSE)
+  expect_true(inherits(summary.sim, "Simulation.Summary"))
+  
+  expect_s3_class(summary.sim@expected.size, "data.frame")
+  
+  # Test new code to check it works in the rare situation that two models have identical AIC values
+  # Cannot test all code but should now run for repeats of the same model and other code is the same.
+  
+  analyses@dfmodel <- list(~1, ~1)
+  analyses@key <- c("hn", "hn")
+  sim <- make.simulation(reps = 1,
+                         design = design,
+                         population.description = pop.descrp,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  sim.check <- run.simulation(sim)
+  
+  expect_equal(sim.check@warnings$message[[1]],
+               "Two or more models had the same information criterion value and the same number of parameters, taking the first model of: models 1, 2")
+  
+})
+
