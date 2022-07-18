c4e330bcf895816050e3a02361d30f6c504a0a11
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2022-07-18 04:06:58 +0100

    Add ability to read in transects from shapefile
    Reference issue #48

---
+++ R/read.line.transects.R
@@ -0,0 +1,73 @@
+read.line.transects <- function(filename, design, warnings = list(), rep = NA){
+  # Read in shapefile
+  # Identify type of shapefile (point/linestring/multilinestring)
+  # Check shapefile type matches with specified design
+  
+  # Information extracted from design (general to all)
+  # - design
+  # - strata.area   
+  # - effort.allocation 
+  # - spacing       
+  # - design.angle 
+  # - edge.protocol 
+  # Information extracted from design (segment)
+  # - seg.length
+  # - seg.threshold
+  
+  # Information obtained from shapefile (general to all)
+  # - samplers 
+  # - cov.area      
+  # - cov.area.polys 
+  # - samp.count
+  # Information obtained from shapefile (line / segment )
+  # - line.length
+  
+  # Information that cannot be obtained
+  # - trackline
+  # - cyclictrackline
+  
+  all.transects <- sf::read_sf(filename)
+  # Get geometry
+  sf.column <- attr(all.transects, "sf_column")
+  sf.geom <- all.transects[[sf.column]]
+  
+  # Check that it is of type point
+  if(!is(sf.geom, "sfc_MULTILINESTRING") && !is(sf.geom, "sfc_LINESTRING")){
+    warnings <- message.handler(warnings, paste("The following file cannot be used, shapefile not of type line / multi-line: ", filename, sep = ""), rep)
+    return(list(transects = NULL, warnings = warnings))
+  }
+  
+  # Process the shapefile in case it came from Distance for Windows
+  all.transects <- process.dist.shapes(all.transects, design@region)
+  
+  # Find covered areas
+  cov.areas <- get.covered.area.lines(all.transects, 
+                                       design@truncation, 
+                                       design@region)
+  
+  # Extract information
+  sampler.count <- cov.areas$sampler.count
+  areas <- cov.areas$area
+  line.length <- cov.areas$line.length
+  cov.area.polys <- cov.areas$polys
+  
+  #Make a survey object
+  transect <- new(Class="Line.Transect", 
+                design = design@design, 
+                lines = all.transects,
+                samp.count = sampler.count, 
+                line.length = line.length,
+                seg.length = numeric(0),
+                effort.allocation = design@effort.allocation, 
+                spacing = design@spacing, 
+                design.angle = design@design.angle, 
+                edge.protocol = design@edge.protocol, 
+                cov.area = areas, 
+                cov.area.polys = cov.area.polys , 
+                strata.area = design@region@area,
+                strata.names = design@region@strata.name,
+                trackline = numeric(0),
+                cyclictrackline = numeric(0)) 
+  
+  return(list(transects = transect, warnings = warnings))
+}
---
+++ R/read.point.transects.R
@@ -0,0 +1,68 @@
+read.point.transects <- function(filename, design, warnings = list(), rep = NA){
+  # Read in shapefile
+  # Identify type of shapefile (point/linestring/multilinestring)
+  # Check shapefile type matches with specified design
+  
+  # Information extracted from design (general to all)
+  # - design
+  # - strata.area   
+  # - effort.allocation 
+  # - spacing       
+  # - design.angle 
+  # - edge.protocol 
+  # Information extracted from design (segment)
+  # - seg.length
+  # - seg.threshold
+  
+  # Information obtained from shapefile (general to all)
+  # - samplers 
+  # - cov.area      
+  # - cov.area.polys 
+  # - samp.count
+  # Information obtained from shapefile (line / segment )
+  # - line.length
+  
+  # Information that cannot be obtained
+  # - trackline
+  # - cyclictrackline
+  
+  all.transects <- sf::read_sf(filename)
+  # Get geometry
+  sf.column <- attr(all.transects, "sf_column")
+  sf.geom <- all.transects[[sf.column]]
+  
+  # Check that it is of type point
+  if(!is(sf.geom, "sfc_POINT")){
+    warnings <- message.handler(warnings, paste("The following file cannot be used, shapefile not of type point: ", filename, sep = ""), rep)
+    return(list(transects = NULL, warnings = warnings))
+  }
+  
+  # Process the shapefile in case it came from Distance for Windows
+  all.transects <- process.dist.shapes(all.transects, design@region)
+  
+  # Find covered areas
+  cov.areas <- get.covered.area.points(all.transects, 
+                                       design@truncation, 
+                                       design@region)
+  
+  # Extract information
+  sampler.count <- cov.areas$sampler.count
+  areas <- cov.areas$area
+  cov.area.polys <- cov.areas$polys
+  
+  #Make a survey object
+  transect <- new(Class="Point.Transect", 
+                design = design@design, 
+                points = all.transects,
+                samp.count = sampler.count, 
+                effort.allocation = design@effort.allocation, 
+                spacing = design@spacing, 
+                design.angle = design@design.angle, 
+                edge.protocol = design@edge.protocol, 
+                cov.area = areas, 
+                cov.area.polys = cov.area.polys , 
+                strata.area = design@region@area,
+                strata.names = design@region@strata.name) 
+  
+  return(list(transects = transect, warnings = warnings))
+}
