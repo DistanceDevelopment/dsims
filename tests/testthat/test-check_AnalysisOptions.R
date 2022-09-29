d9e2d6db8403e265385408115795df5dac1d6639
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2022-09-29 12:11:12 +0100

    Add tests

--- tests/testthat/test-check_AnalysisOptions.R
+++ tests/testthat/test-check_AnalysisOptions.R
@@ -96,3 +96,144 @@ test_that("Can create objects or return correct error / warning messages", {
 
 
 })
+
+test_that("Grouping strata", {
+  
+  outer <- matrix(c(0,0,15,0,15,10,0,10,0,0),ncol=2, byrow=TRUE)
+  hole1 <- matrix(c(2,2,2,3,3,3,3,2,2,2),ncol=2, byrow=TRUE)
+  hole2 <- matrix(c(5,5,5,6,7,6,8,5.5,7,5,5,5),ncol=2, byrow=TRUE)
+  pol1 <- sf::st_polygon(list(outer, hole1*1.5, hole2))
+  pol2 <- sf::st_polygon(list(outer + 15, hole2*1.5 + 12))
+  pol3 <- sf::st_polygon(list(outer + 30, hole2*2.5 + 20))
+  sfc <- sf::st_sfc(pol1,pol2,pol3)
+  strata.names <- c("SW", "central", "NE")
+  mp1 <- sf::st_sf(strata = strata.names, geom = sfc)
+  
+  region <- make.region(region.name = "study.area", 
+                        strata.name = strata.names, 
+                        shape = mp1)
+  plot(region)
+  
+  density <- make.density(region,
+                          x.space = 0.5,
+                          constant = rep(1,3))
+  
+  design <- make.design(region,
+                        design = "systematic",
+                        samplers = 30,
+                        truncation = 0.5)
+  
+  pop.descript <- make.population.description(region,
+                                              density,
+                                              N = c(200,200,200))
+  
+  detect <- make.detectability(scale.param = 0.25,
+                               truncation = 0.5)
+  
+  grp.strat <- data.frame(design.id = c("SW", "central", "NE"),
+                          analysis.id = rep("global",3))
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1),
+                               key = "hn",
+                               truncation = 0.5,
+                               group.strata = grp.strat)
+  
+  sim <- make.simulation(reps = 3,
+                         design = design,
+                         population.description = pop.descript,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  
+  test <- run.simulation(sim)
+  tmp <- summary(test, description.summary = FALSE)
+  
+  expect_null(tmp@expected.size$Truth)
+  true.individsN <- 600
+  expect_equal(tmp@individuals$N$Truth, true.individsN)
+  
+  
+  # Test when cluster size is in the simulation
+  covs <- list()
+  covs$size <- list(list(distribution = "poisson", lambda = 25))
+  
+  pop.descript <- make.population.description(region,
+                                              density,
+                                              covariates = covs,
+                                              N = c(200,200,200))
+  
+  sim <- make.simulation(reps = 3,
+                         design = design,
+                         population.description = pop.descript,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  
+  test <- run.simulation(sim)
+  tmp <- summary(test, description.summary = FALSE)
+  
+  true.es <-25
+  true.clusterN <-  600
+  expect_equal(tmp@expected.size$Truth, true.es)
+  expect_equal(tmp@clusters$N$Truth,true.clusterN)
+  expect_equal(tmp@individuals$N$Truth, true.es*true.clusterN)
+  
+  # Test when only some are grouped
+  grp.strat <- data.frame(design.id = c("SW", "central", "NE"),
+                          analysis.id = c(rep("S_Central",2), "NE"))
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1),
+                               key = "hn",
+                               truncation = 0.5,
+                               group.strata = grp.strat)
+  
+  sim <- make.simulation(reps = 3,
+                         design = design,
+                         population.description = pop.descript,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  
+  test <- run.simulation(sim)
+  tmp <- summary(test, description.summary = FALSE)
+  
+  true.es <- c(25,25,25)
+  true.clusterN <-  c(200,400,600)
+  expect_equal(tmp@expected.size$Truth, true.es)
+  expect_equal(tmp@clusters$N$Truth,true.clusterN)
+  expect_equal(tmp@individuals$N$Truth, true.es*true.clusterN)
+  
+  
+  # Modify cluster size values to check 
+  
+  covs <- list()
+  covs$size <- list(list(distribution = "poisson", lambda = 25),
+                    list(distribution = "poisson", lambda = 50),
+                    list(distribution = "poisson", lambda = 100))
+  
+  pop.descript <- make.population.description(region,
+                                              density,
+                                              covariates = covs,
+                                              N = c(200,300,500))
+  
+  grp.strat <- data.frame(design.id = c("SW", "central", "NE"),
+                          analysis.id = c(rep("S_Central",2), "NE"))
+  
+  analyses <- make.ds.analysis(dfmodel = list(~1),
+                               key = "hn",
+                               truncation = 0.5,
+                               group.strata = grp.strat)
+  
+  
+  sim <- make.simulation(reps = 3,
+                         design = design,
+                         population.description = pop.descript,
+                         detectability = detect,
+                         ds.analysis = analyses)
+  
+  test <- run.simulation(sim)
+  tmp <- summary(test, description.summary = FALSE)
+  
+  true.es <- c(100, 25*0.4 + 0.6*50, 25*0.2 + 0.3*50 + 0.5*100)
+  true.clusterN <-  c(500,500,1000)
+  expect_equal(tmp@expected.size$Truth, true.es)
+  expect_equal(tmp@clusters$N$Truth,true.clusterN)
+  expect_equal(tmp@individuals$N$Truth, true.es*true.clusterN)
+})
